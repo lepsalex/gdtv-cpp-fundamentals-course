59335b5e1c297ff59c5eb516f02366ea15911d25
Alex Lepsa <lepsalex@gmail.com>
2024-11-02 18:14:02 -0400

    axe game project complete

--- axe-game/src/main.cpp
+++ axe-game/src/main.cpp
@@ -1,30 +1,80 @@
 #include "raylib.h"
 
-#define SCREEN_WIDTH (800)
-#define SCREEN_HEIGHT (450)
+int main(void) {
+    // window specs
+    static constexpr int screen_width = 800;
+    static constexpr int screen_height = 450;
+    static const char *window_title = "Axe Game";
 
-#define WINDOW_TITLE "Window title"
+    // circle
+    int circle_x = screen_width / 2 - 200;
+    int circle_y = screen_height / 2;
+    int circle_r = 25;
 
-int main(void)
-{
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    // circle edges
+    int l_circle_x = circle_x - circle_r;
+    int r_circle_x = circle_x + circle_r;
+    int u_circle_y = circle_y - circle_r;
+    int b_circle_y = circle_y + circle_r;
+
+    // axe
+    int axe_x = 400;
+    int axe_y = 0;
+    int axe_length = 50;
+
+    // axe edges
+    int l_axe_x = axe_x;
+    int r_axe_x = axe_x + axe_length;
+    int u_axe_y = axe_y;
+    int b_axe_y = axe_y + axe_length;
+    int direction = 10;
+
+    // collision
+    bool collision_with_axe = false;
+
+    InitWindow(screen_width, screen_height, window_title);
     SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture("resources/test.png");
-
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(WHITE);
 
-        ClearBackground(RAYWHITE);
+        // Game logic begins
+        if (collision_with_axe) {
+            DrawText("Game Over!", 400, 200, 20, RED);
+        } else {
+            // Update collider edges
+            l_circle_x = circle_x - circle_r;
+            r_circle_x = circle_x + circle_r;
+            u_circle_y = circle_y - circle_r;
+            b_circle_y = circle_y + circle_r;
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
 
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
+            // Detect collision with axe
+            collision_with_axe = (b_axe_y >= u_circle_y
+                                  && u_axe_y <= b_circle_y
+                                  && l_axe_x <= r_circle_x
+                                  && r_axe_x >= l_circle_x);
 
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
+            DrawCircle(circle_x, circle_y, circle_r, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            // Move Axe
+            axe_y += direction;
+            if (axe_y + axe_length >= screen_height || axe_y <= 0) {
+                direction *= -1;
+            }
+
+            // Input
+            if (IsKeyDown(KEY_D) && circle_x + circle_r < screen_width)
+                circle_x += 10;
+            if (IsKeyDown(KEY_A) && circle_x - circle_r > 0)
+                circle_x -= 10;
+        }
+        // Game logic ends
 
         EndDrawing();
     }
--- raylib-cmake-template-master/src/main.cpp
+++ raylib-cmake-template-master/src/main.cpp
@@ -5,15 +5,13 @@
 
 #define WINDOW_TITLE "Window title"
 
-int main(void)
-{
+int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(60);
 
     Texture2D texture = LoadTexture("resources/test.png");
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
@@ -22,7 +20,7 @@ int main(void)
         const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
+        const char *text = "OMG! IT WORKS!";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
